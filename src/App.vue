876f9ddd6d0f7b0fa44c6668dc00aa39b144ff26
chrisutpg <chris@uptopargolf.com>
2019-01-25 15:30:11 -0500

    Breakpoint issue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+
+    <v-app>
+
+      <v-content>
+        <v-container fluid>
+          <v-layout row>
+            <v-flex md12 lg10 xl10>
+              {{ this.$vuetify.breakpoint.mdAndDown }}
+            </v-flex>
+            <v-flex md12 lg2 xl2>
+              {{ this.$vuetify.breakpoint.smAndDown }}
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <v-footer app>2019 FranchiseFaves.com</v-footer>
+
+    </v-app>
+
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+
+// Override default theme for Vuetify
+Vue.use(Vuetify, {
+  iconfont: 'mdi',
+  theme: {
+    "primary": "#673ab7",
+    "secondary": "#757575",
+    "accent": "#d500f9",
+    "error": "#ff1744",
+    "info": "#d500f9",
+    "success": "#4caf50",
+    "warning": "#ff9100"
+  }
+})
 
 new Vue({
   router,
